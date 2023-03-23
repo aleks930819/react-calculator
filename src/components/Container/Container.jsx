e5ed0c6e7477615c276ebde2b5ad3d23fadc15e9
Aleksandar Atanasov <aleks930819@gmail.com>
2023-03-23 20:32:15 +0200

    changed style

--- src/components/Container/Container.jsx
+++ src/components/Container/Container.jsx
@@ -1,5 +1,9 @@
 const Container = ({ children }) => {
-  return <div className="calcuator-grid">{children}</div>;
+  return (
+    <div className="center">
+      <div className="calcuator-grid">{children}</div>;
+    </div>
+  );
 };
 
 export default Container;
